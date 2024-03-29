d0b8185c2d68933f5b034df61d2188d284205ae6
Théo Duflos <theo.duflos.yokoko@gmail.com>
2024-03-29 16:30:12 +0100

    feat: :sparkles: add crud enpoint for brewery

---
+++ minimal_api/src/main/java/com/zythologue/minimal_api/Controller/BreweryController.java
@@ -0,0 +1,85 @@
+package com.zythologue.minimal_api.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.zythologue.minimal_api.Model.BeerDTO;
+import com.zythologue.minimal_api.Model.BreweryDTO;
+
+@RestController
+public class BreweryController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("brewery")
+    public List<BreweryDTO> getAllBrewery() {
+        String sql = "SELECT * FROM Brewery";
+
+        List<BreweryDTO> brewerys = jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> {
+                    BreweryDTO brewery = new BreweryDTO();
+                    brewery.setName(rs.getString("zybr_name"));
+                    brewery.setCountry(rs.getString("zybr_country"));
+                    brewery.setWebsite(rs.getString("zybr_website"));
+
+                    return brewery;
+                });
+
+        return brewerys;
+    }
+
+    @GetMapping("brewery/{id}")
+    public List<BreweryDTO> getOneBrewery(@PathVariable int id) {
+        String sql = "SELECT zybr_name, zybr_country, zybr_website FROM Brewery WHERE zybr_id = ?";
+
+        List<BreweryDTO> brewery = jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> {
+                    BreweryDTO breweryQuery = new BreweryDTO();
+                    breweryQuery.setName(rs.getString("zybr_name"));
+                    breweryQuery.setCountry(rs.getString("zybr_country"));
+                    breweryQuery.setWebsite(rs.getString("zybr_website"));
+                    return breweryQuery;
+                },
+                id);
+        return brewery;
+    }
+
+    @PostMapping("/brewery")
+    public void insertBrewery(@RequestBody BreweryDTO breweryDTO) {
+        jdbcTemplate.update(
+            "INSERT INTO brewery (zybr_name, zybr_country, zybr_website) VALUES (?, ?, ?)",
+            breweryDTO.getName(),
+            breweryDTO.getCountry(),
+            breweryDTO.getWebsite()
+        );
+    }
+
+    @PutMapping("brewery/{id}")
+    public void updateBrewery(@PathVariable int id, @RequestBody BreweryDTO breweryDTO) {
+        jdbcTemplate.update(
+            "UPDATE brewery SET zybr_name = ?, zybr_country = ?, zybr_website = ? WHERE zybr_id = ?",
+            breweryDTO.getName(),
+            breweryDTO.getCountry(),
+            breweryDTO.getWebsite(),
+            id
+        );
+    }
+    
+    @DeleteMapping("brewery/{id}")
+    public void deleteBeer(@PathVariable int id) {
+        jdbcTemplate.update("DELETE FROM brewery WHERE zybr_id = ?", id);
+    }
+}
---
+++ minimal_api/src/main/java/com/zythologue/minimal_api/Model/BreweryDTO.java
@@ -0,0 +1,25 @@
+package com.zythologue.minimal_api.Model;
+
+public class BreweryDTO {
+    public String name;
+    public String country;
+    public String website;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getWebsite() {
+        return website;
+    }
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}

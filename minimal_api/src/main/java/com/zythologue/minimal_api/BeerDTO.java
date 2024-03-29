52ca10e034649cf12076a6f948a2c4a640ccc275
Théo Duflos <theo.duflos.yokoko@gmail.com>
2024-03-29 15:06:10 +0100

    feat: :sparkles: add auto build in dockerfile for java spring app

--- minimal_api/src/main/java/com/zythologue/minimal_api/BeerDTO.java
+++ minimal_api/src/main/java/com/zythologue/minimal_api/BeerDTO.java
@@ -1,64 +1,50 @@
 package com.zythologue.minimal_api;
 
-
-public class Beer {
-    
-
-    public int id;
+public class BeerDTO {
     public String name;
     public String description;
     public float abv;
-    public int Brewery;
+    public int brewery;
     public int category;
-    public java.sql.Timestamp created;
-    public java.sql.Timestamp updated;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public float getAbv() {
         return abv;
     }
+
     public void setAbv(float abv) {
         this.abv = abv;
     }
+
     public int getBrewery() {
-        return Brewery;
+        return brewery;
     }
+
     public void setBrewery(int brewery) {
-        Brewery = brewery;
+        this.brewery = brewery;
     }
+
     public int getCategory() {
         return category;
     }
+
     public void setCategory(int category) {
         this.category = category;
-    }
-    public java.sql.Timestamp getCreated() {
-        return created;
-    }
-    public void setCreated(java.sql.Timestamp created) {
-        this.created = created;
-    }
-    public java.sql.Timestamp getUpdated() {
-        return updated;
-    }
-    public void setUpdated(java.sql.Timestamp updated) {
-        this.updated = updated;
     }
 
 }
--- minimal_api/src/main/java/com/zythologue/minimal_api/Controller/BeerController.java
+++ minimal_api/src/main/java/com/zythologue/minimal_api/Controller/BeerController.java
@@ -3,35 +3,83 @@ package com.zythologue.minimal_api.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zythologue.minimal_api.Beer;
+import com.zythologue.minimal_api.BeerDTO;
+
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class BeerController {
 
-    
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+
     @GetMapping("/beers")
     public List<Beer> getAllBeers() {
-        return jdbcTemplate.query(
-                "SELECT * FROM beer",
-                (rs, rowNum) -> new Beer(
-                        rs.getInt("zyb_id"),
-                        rs.getString("zyb_name"),
-                        rs.getString("zyb_description"),
-                        rs.getInt("zyb_category"),
-                        rs.getFloat("zyb_abv"),
-                        rs.getInt("zyb_brewery"),
-                        rs.getTimestamp("zyb_created_at"),
-                        rs.getTimestamp("zyb_updated_at")
-                )
-        );
+        String sql = "SELECT * FROM beer";
+
+        List<Beer> beers = jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> {
+                    Beer beer = new Beer();
+                    beer.setId((rs.getInt("zyb_id")));
+                    beer.setName(rs.getString("zyb_name"));
+                    beer.setDescription(rs.getString("zyb_description")); // Assuming typo in original code
+                    beer.setCategory(rs.getInt("zyb_category"));
+                    beer.setAbv(rs.getFloat("zyb_abv"));
+                    beer.setBrewery(rs.getInt("zyb_brewery"));
+                    beer.setCreated(rs.getTimestamp("zyb_created_at"));
+                    beer.setUpdated(rs.getTimestamp("zyb_updated_at"));
+                    return beer;
+                });
+
+        return beers;
+    }
+
+    @GetMapping("/beer/{id}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable int id) {
+        String sql = "SELECT zyb_name, zyb_description, zyb_category, zyb_abv, zyb_brewery FROM beer WHERE zyb_id = ?";
+
+        List<BeerDTO> beers = jdbcTemplate.query(
+                sql,
+                preparedStatement -> preparedStatement.setInt(1, id),
+                (rs, rowNum) -> {
+                    BeerDTO beerDTO = new BeerDTO();
+                    beerDTO.setName(rs.getString("zyb_name"));
+                    beerDTO.setDescription(rs.getString("zyb_description")); // Assuming typo in original code
+                    beerDTO.setCategory(rs.getInt("zyb_category"));
+                    beerDTO.setAbv(rs.getFloat("zyb_abv"));
+                    beerDTO.setBrewery(rs.getInt("zyb_brewery"));
+                    return beerDTO;
+                });
+
+        if (beers.isEmpty()) {
+            return ResponseEntity.notFound().build(); // Handle no beer found
+        }
+
+        return ResponseEntity.ok(beers.get(0));
+
+    }
+
+    @PostMapping("/beer")
+    public void updateBeer(@RequestBody BeerDTO beer) {
+        jdbcTemplate.update(
+            "INSERT INTO beer (zyb_name, zyb_description, zyb_abv, zyb_brewery, zyb_category) VALUES (?, ?, ?, ?, ?)",
+            beer.getName(),
+            beer.getDescription(),
+            beer.getAbv(),
+            beer.getBrewery(),
+            beer.getCategory());
     }
     
-
 }
